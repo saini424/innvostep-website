63dc54d76a3bc30e51aa87f7e12775c6c5705f9d
Shravan Sankhla <shravansankhla@Shravans-MacBook-Air.local>
2026-05-19 16:20:34 +0530

    added sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,12 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://innvostep.site",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    },
+  ];
+}
