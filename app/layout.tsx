15b9bb3749e54090bcc4bb4b1a0c3f1ffce6c217
Shravan Sankhla <shravansankhla@Shravans-MacBook-Air.local>
2026-05-19 16:01:44 +0530

    fixed google verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,14 @@
 import "./globals.css";
+import type { Metadata } from "next";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Innvostep | Startup Learning Platform",
   description:
     "Learn entrepreneurship, startup execution and business growth.",
+
+  verification: {
+    google: "CttwmsORfBm1DVOc5jmQPLNlx1w6CXrAwdZScnPo5q4",
+  },
 };
 
 export default function RootLayout({
